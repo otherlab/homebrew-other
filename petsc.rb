6a382186ff043675353908cb99054a69815acd37
Ryan Alexander <ryan@onecm.com>
2013-10-30 13:43:19 -0700

    Add petsc formula

---
+++ petsc.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Petsc < Formula
+  homepage 'http://www.mcs.anl.gov/petsc/index.html'
+  url 'http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.4.2.tar.gz'
+  sha1 '9ba201c73ca85e01da37176b7bbf80a75756c515'
+
+  depends_on :mpi => :cc
+  depends_on :fortran
+
+  def install
+    ENV.deparallelize
+    system "./configure", "--with-debugging=0", "--with-shared-libraries=1", "--with-fortran=0", "--prefix=#{prefix}"
+    system "make all"
+    system "make test"
+    system "make install"
+  end
+end
